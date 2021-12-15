af3b6f0eb8c50f8cebff3e70363850031e88abe1
Slava Levkovich <slavlen1999@gmail.com>
2021-12-15 13:53:00 +0300

    cors fix

--- src/service/service.ts
+++ src/service/service.ts
@@ -3,4 +3,5 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 export const service = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_URI,
   cache: new InMemoryCache(),
+  credentials: "same-origin",
 });
